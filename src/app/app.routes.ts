f64057e46f8e46affe008614d49bfde903fc5562
Alex Mwaura <alexmwaura36@gmail.com>
2025-05-01 15:04:20 +0300

    add routing for the create order component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,10 @@
 import { Routes } from '@angular/router';
 import { OrderListComponent } from './orders/pages/order-list.component';
+import { CreateOrderComponent } from './orders/pages/create-order/create-order.component';
 
 export const routes: Routes = [ 
     { path: '', redirectTo: 'orders', pathMatch: 'full' },
-    { path: 'orders', component: OrderListComponent }
+    { path: 'orders', component: OrderListComponent },
+    { path: 'orders/new', component: CreateOrderComponent }
+
   ];
